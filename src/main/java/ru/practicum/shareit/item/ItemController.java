1ee9fb8c7d42602fdced543274a67c2d4060e3eb
Ruslan Buldakov <rsbuldakov@yandex.ru>
2023-01-12 14:52:40 +0400

    style: remove TODO

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -16,9 +16,6 @@ import ru.practicum.shareit.item.service.ItemService;
 
 import java.util.List;
 
-/**
- * TODO Sprint add-controllers.
- */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/items")
