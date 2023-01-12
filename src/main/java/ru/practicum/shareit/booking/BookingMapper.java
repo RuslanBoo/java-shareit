b671703b4ae737d474cc549bed5deafd62cefb08
Ruslan Buldakov <rsbuldakov@yandex.ru>
2023-01-12 20:47:56 +0400

    refactor: add mapper to other classes

--- src/main/java/ru/practicum/shareit/booking/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/BookingMapper.java
@@ -1,32 +1,13 @@
 package ru.practicum.shareit.booking;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.model.Booking;
 
-@Component
-@RequiredArgsConstructor
-public class BookingMapper {
-    public BookingDto toDto(Booking booking) {
-        return BookingDto.builder()
-                .id(booking.getId())
-                .item(booking.getItem())
-                .booker(booking.getBooker())
-                .start(booking.getStart())
-                .end(booking.getEnd())
-                .status(booking.getStatus())
-                .build();
-    }
+@Mapper
+public interface BookingMapper {
+    public BookingDto toDto(Booking booking);
 
-    public Booking fromDto(BookingDto bookingDto) {
-        return Booking.builder()
-                .item(bookingDto.getItem())
-                .booker(bookingDto.getBooker())
-                .start(bookingDto.getStart())
-                .end(bookingDto.getEnd())
-                .status(bookingDto.getStatus())
-                .build();
-    }
+    public Booking fromDto(BookingDto bookingDto);
 }
 
--- src/main/java/ru/practicum/shareit/request/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestMapper.java
@@ -1,24 +1,12 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.model.ItemRequest;
 
-@Component
-public class ItemRequestMapper {
-    public ItemRequestDto toDto(ItemRequest itemRequest) {
-        return ItemRequestDto.builder()
-                .id(itemRequest.getId())
-                .description(itemRequest.getDescription())
-                .requestor(itemRequest.getRequestor())
-                .created(itemRequest.getCreated())
-                .build();
-    }
+@Mapper
+public interface ItemRequestMapper {
+    ItemRequestDto toDto(ItemRequest itemRequest);
 
-    public ItemRequest fromDto(ItemRequestDto itemRequestDto) {
-        return ItemRequest.builder()
-                .description(itemRequestDto.getDescription())
-                .requestor(itemRequestDto.getRequestor())
-                .build();
-    }
+    ItemRequest fromDto(ItemRequestDto itemRequestDto);
 }
