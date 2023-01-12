d6e16e58d43ed866d8fb74592e2391fef572521a
Ruslan Buldakov <rsbuldakov@yandex.ru>
2023-01-12 14:53:56 +0400

    style: remove single char variables name

--- src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -16,30 +16,30 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleBadRequestException(final BadRequestException e) {
-        log.error("Error code: 400.", e);
-        return new ErrorResponse(e.getMessage());
+    public ErrorResponse handleBadRequestException(final BadRequestException badRequestException) {
+        log.error("Error code: 400.", badRequestException);
+        return new ErrorResponse(badRequestException.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleDataNotFoundException(final DataNotFoundException e) {
-        log.error("Error code: 404.", e);
-        return new ErrorResponse(e.getMessage());
+    public ErrorResponse handleDataNotFoundException(final DataNotFoundException dataNotFoundException) {
+        log.error("Error code: 404.", dataNotFoundException);
+        return new ErrorResponse(dataNotFoundException.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse handleConflictException(final ConflictException e) {
-        log.error("Error code: 409.", e);
-        return new ErrorResponse(e.getMessage());
+    public ErrorResponse handleConflictException(final ConflictException conflictException) {
+        log.error("Error code: 409.", conflictException);
+        return new ErrorResponse(conflictException.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleInternalServerError(final Throwable e) {
-        log.error("Error code: 500.", e);
-        return new ErrorResponse(e.getMessage());
+    public ErrorResponse handleInternalServerError(final Throwable throwable) {
+        log.error("Error code: 500.", throwable);
+        return new ErrorResponse(throwable.getMessage());
     }
 
 }
