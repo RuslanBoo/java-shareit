e78e9d7ed14bace0d07882271bca270461bc5c7b
Ruslan <rsbuldakov@yandex.ru>
2023-04-01 21:19:14 +0400

    refactor: create base template with app.props

--- src/main/java/ru/practicum/shareit/error/model/DataNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.error.model;
-
-public class DataNotFoundException extends RuntimeException {
-    public DataNotFoundException(String message) {
-        super(message);
-    }
-}
